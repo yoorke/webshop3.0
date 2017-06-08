8befe40d2fa0937be013d42f9c9fa59e3d8240ab
Dejan Jurisic <dejanjurisic@yahoo.com>
2017-06-08 23:10:01 +0200

    webShopAdmin

---
+++ webShopAdmin/app/components/base/controllers/defaultItemController.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'user strict';
+
+    angular.module('webShopAdmin').controller('DefaultItemController', DefaultItemController);
+
+    DefaultItemController.$inject = ['$controller', 'GlobalService'];
+
+    function DefaultItemController($controller, GlobalService) {
+        var vm = this;
+
+        $controller('InitController', { vm: vm });
+        $controller('BaseGetByIDController', { vm: vm });
+        $controller('BaseSaveController', { vm: vm });
+        $controller('CommonController', { vm: vm });
+
+        init();
+
+        function init() {
+            vm.BaseGetItemByID(vm.modelName, vm.defaultID).then(function (response) {
+                vm.item = response.data;
+            })
+        }
+    }
+})()
