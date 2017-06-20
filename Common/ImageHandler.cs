fa24d59fd965aa611d195ce9ace81f53236e535f
Dejan Jurisic <dejanjurisic@yahoo.com>
2017-06-20 08:28:07 +0200

    webShopAdmin

--- Common/ImageHandler.cs
+++ Common/ImageHandler.cs
@@ -5,14 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing.Imaging;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace Common
 {
     public class ImageHandler
     {
-        public void SaveImage(Image image, int width, int height, string path)
+        public void SaveImage(Image image, ImageFormat format, string path)
         {
+            using (Bitmap bitmap = new Bitmap(image))
+            {
+                bitmap.Save(path, format);
+            }
+        }
 
+        public void SaveThumb(Image image, ImageFormat format, int width, int height, string path, string sufix)
+        {
+            string extenstion = path.Substring(path.LastIndexOf('.'));
+            string imageName = path.Substring(0, path.LastIndexOf('.'));
+
+            Image thumb = createThumb(image, width, height);
+            thumb.Save(imageName + "-" + (sufix != string.Empty ? "-" + sufix : string.Empty) + extenstion);
         }
 
         public string GetExtension(string type)
@@ -35,6 +48,56 @@ namespace Common
                 case "image/svg": return ImageFormat.Icon;
             }
             return null;
+        }
+
+        private Image createThumb(Image imgPhoto, int Width, int Height)
+        {
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+            int sourceX = 0;
+            int sourceY = 0;
+            int destX = 0;
+            int destY = 0;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)Width / (float)sourceWidth);
+            nPercentH = ((float)Height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+            {
+                nPercent = nPercentH;
+                destX = System.Convert.ToInt16((Width -
+                              (sourceWidth * nPercent)) / 2);
+            }
+            else
+            {
+                nPercent = nPercentW;
+                destY = System.Convert.ToInt16((Height -
+                              (sourceHeight * nPercent)) / 2);
+            }
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap bmPhoto = new Bitmap(Width, Height,
+                              PixelFormat.Format24bppRgb);
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution,
+                             imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.Clear(Color.White);
+            grPhoto.InterpolationMode =
+                    InterpolationMode.HighQualityBicubic;
+
+            grPhoto.DrawImage(imgPhoto,
+                new Rectangle(destX, destY, destWidth, destHeight),
+                new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight),
+                GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            return bmPhoto;
         }
 
     }
--- eshopAPI/Controllers/CommonController.cs
+++ eshopAPI/Controllers/CommonController.cs
@@ -10,6 +10,7 @@ using System.Drawing.Imaging;
 using System.Drawing;
 using System.Web.Hosting;
 using Common;
+using System.Web;
 
 namespace eshopAPI.Controllers
 {
@@ -32,16 +33,18 @@ namespace eshopAPI.Controllers
                         {
                             if (content.Headers.ContentType.MediaType == "image/png" || content.Headers.ContentType.MediaType == "image/jpeg" || content.Headers.ContentType.MediaType == "image/svg")
                             {
-                                extension = new ImageHandler().GetExtension(content.Headers.ContentType.MediaType);
-                                ImageFormat format = new ImageHandler().GetImageFormat(content.Headers.ContentType.MediaType);
+                                ImageHandler imageHandler = new ImageHandler();
+                                extension = imageHandler.GetExtension(content.Headers.ContentType.MediaType);
+                                ImageFormat format = imageHandler.GetImageFormat(content.Headers.ContentType.MediaType);
                                 Image image = Image.FromStream(stream);
                                 var name = content.Headers.ContentDisposition.Name;
                                 string filepath = HostingEnvironment.MapPath("~/images");
                                 string fullpath = Path.Combine(filepath, filename + extension);
-                                using (Bitmap bitmap = new Bitmap(image))
-                                {
-                                    bitmap.Save(fullpath, format);
-                                }
+                                //using (Bitmap bitmap = new Bitmap(image))
+                                //{
+                                //bitmap.Save(fullpath, format);
+                                //}
+                                imageHandler.SaveImage(image, format, fullpath);
                             }
                             else
                                 throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadGateway));
@@ -54,7 +57,33 @@ namespace eshopAPI.Controllers
                 throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadGateway));
         }
 
-        
+        [HttpPost]
+        [ActionName("UploadImage")]
+        public HttpResponseMessage UploadImage()
+        {
+            var httpRequest = HttpContext.Current.Request;
+
+            //HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+            var file = httpRequest.Files[0];
+
+            if(file != null && file.ContentLength > 0)
+            {
+                IList<string> allowedExtensions = new List<string> { ".jpg", ".png" };
+                string extension = file.FileName.Substring(file.FileName.LastIndexOf(".")).ToLower();
+                if(!allowedExtensions.Contains(extension))
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest);
+                }
+                else
+                {
+                    string fullpath = HttpContext.Current.Server.MapPath("~/images/" + file.FileName);
+                    file.SaveAs(fullpath);
+
+                    return Request.CreateResponse(HttpStatusCode.Created, file.FileName);
+                }
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest);
+        }
 
         
     }
